4b28bb6f08cb79cbe0d3fa1d1f270f7735388c02
jcordry <julien.cordry@gmail.com>
2024-10-08 17:34:54 +0100

    Added interactive UI

--- app/src/main/java/uk/ac/tees/scedt/mgd/u0026939/pokemoncollection/MainActivity.kt
+++ app/src/main/java/uk/ac/tees/scedt/mgd/u0026939/pokemoncollection/MainActivity.kt
@@ -3,45 +3,63 @@ package uk.ac.tees.scedt.mgd.u0026939.pokemoncollection
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import uk.ac.tees.scedt.mgd.u0026939.pokemoncollection.ui.theme.PokemonCollectionTheme
+import androidx.compose.ui.unit.dp
+import uk.ac.tees.mgd.u0026939.recompose.ui.theme.RecomposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            PokemonCollectionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            HelloScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HelloScreen() {
+    var name by rememberSaveable {
+        mutableStateOf("")
+    }
+    Hello(name) {it -> name = it}
 }
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview() {
-    PokemonCollectionTheme {
-        Greeting("Android")
+fun Hello(name: String, onNameChange : (String) -> Unit) {
+    Column {
+        if (name.isNotEmpty()) {
+            Text(
+                text = "Hi $name",
+                modifier = Modifier.padding(bottom = 8.dp),
+                style = MaterialTheme.typography.headlineMedium
+            )
+        }
+        OutlinedTextField(
+            value = name,
+            onValueChange = onNameChange,
+            label = { Text(text = "name") },
+            modifier = Modifier.padding(8.dp)
+        )
+        Button(
+            onClick = { onNameChange("") },
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text(text = "Clear!")
+        }
     }
 }
+
